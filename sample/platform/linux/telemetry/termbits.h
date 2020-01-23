db9fde809b393949119cbc05186f6b8adae7c0bb
Ihor Suzdaliev <swindows101@gmail.com>
2020-01-23 19:02:22 +0200

    adsfgadf

--- sample/platform/linux/telemetry/termbits.h
+++ sample/platform/linux/telemetry/termbits.h
@@ -9,15 +9,15 @@ typedef unsigned int	speed_t;
 typedef unsigned int	tcflag_t;
 
 #define NCCS 19
-struct termios {
-	tcflag_t c_iflag;		/* input mode flags */
-	tcflag_t c_oflag;		/* output mode flags */
-	tcflag_t c_cflag;		/* control mode flags */
-	tcflag_t c_lflag;		/* local mode flags */
-	cc_t c_line;			/* line discipline */
-	cc_t c_cc[NCCS];		/* control characters */
-};
 
+//struct termios {
+//	tcflag_t c_iflag;		/* input mode flags */
+//	tcflag_t c_oflag;		/* output mode flags */
+//	tcflag_t c_cflag;		/* control mode flags */
+//	tcflag_t c_lflag;		/* local mode flags */
+//	cc_t c_line;			/* line discipline */
+//	cc_t c_cc[NCCS];		/* control characters */
+//}; 
 struct termios2 {
 	tcflag_t c_iflag;		/* input mode flags */
 	tcflag_t c_oflag;		/* output mode flags */
