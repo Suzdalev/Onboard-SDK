ba0fc7d22b16fc789648d428fad08282af846a6f
Ihor Suzdaliev <swindows101@gmail.com>
2020-01-31 17:57:44 +0200

    pwm3

--- sample/platform/linux/telemetry/main.cpp
+++ sample/platform/linux/telemetry/main.cpp
@@ -31,27 +31,80 @@
 #include "telemetry_sample.hpp"
 #include <thread>
 #include <iostream>
-#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#define PAGE_SIZE (4*1024)
+#define BLOCK_SIZE (4*1024)
+#define PWM_PIN 1
+int  mem_fd;
+void *gpio_map;
+volatile unsigned *gpio;
+
+
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
+
+#define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
+
+#define GPIO_PULL *(gpio+37) // Pull up/pull down
+#define GPIO_PULLCLK0 *(gpio+38) // Pull up/pull down clock
+
 
 
 using namespace DJI::OSDK;
 using namespace DJI::OSDK::Telemetry;
-using namespace std;
+
 
 
 
 int channelGV = 0;
+void setup_io()
+{
+   /* open /dev/mem */
+   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+      printf("can't open /dev/mem \n");
+      exit(-1);
+   }
 
+   /* mmap GPIO */
+   gpio_map = mmap(
+      NULL,             //Any adddress in our space will do
+      BLOCK_SIZE,       //Map length
+      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
+      MAP_SHARED,       //Shared with other processes
+      mem_fd,           //File to map
+      GPIO_BASE         //Offset to GPIO peripheral
+   );
+
+   close(mem_fd); //No need to keep mem_fd open after mmap
+
+   if (gpio_map == MAP_FAILED) {
+      printf("mmap error %d\n", (int)gpio_map);//errno also set!
+      exit(-1);
+   }
+
+   // Always use volatile pointer!
+   gpio = (volatile unsigned *)gpio_map;
+
+
+} // setup_io
 
 void send_control(int& channelGV)
 {
-  wiringPiSetup();
-   pinMode(1, OUTPUT);
+  INP_GPIO(PWM_PIN);
+  OUT_GPIO(PWM_PIN);
   while(true){
    
-    digitalWrite(1, HIGH);
+    GPIO_SET = 1<<PWM_PIN;
     usleep(channelGV+476);
-    digitalWrite(1, LOW);
+    GPIO_CLR = 1<<PWM_PIN;
     usleep(20000);
   }
 
@@ -63,7 +116,7 @@ int main(int argc, char** argv)
   // Setup OSDK.
   LinuxSetup linuxEnvironment(argc, argv);
   Vehicle*   vehicle = NULL;
-
+  setup_io();
   do{
 
       vehicle = linuxEnvironment.getVehicle();
