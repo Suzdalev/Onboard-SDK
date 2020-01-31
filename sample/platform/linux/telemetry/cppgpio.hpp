78523c7659402e346231cae25faed54b1a2e730c
Ihor Suzdaliev <swindows101@gmail.com>
2020-01-31 15:38:12 +0200

    sdf

--- sample/platform/linux/telemetry/cppgpio.hpp
+++ sample/platform/linux/telemetry/cppgpio.hpp
@@ -10,11 +10,11 @@
 #ifndef cppgpio_hpp_AJGSFHGCOJAISLHVZAGJSVBAUKNV
 #define cppgpio_hpp_AJGSFHGCOJAISLHVZAGJSVBAUKNV
 
-#include <cppgpio/gpio.hpp>
-#include <cppgpio/i2c.hpp>
-#include <cppgpio/buttons.hpp>
-#include <cppgpio/lcd.hpp>
-#include <cppgpio/output.hpp>
+#include "cppgpio/gpio.hpp"
+#include "ppgpio/i2c.hpp"
+#include "cppgpio/buttons.hpp"
+#include "cppgpio/lcd.hpp"
+#include "cppgpio/output.hpp"
 
 
 #endif /* cppgpio_hpp */
