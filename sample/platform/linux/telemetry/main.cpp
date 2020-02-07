f524712b8cc8f64a96d0631075c4d1f9d27ad413
Ihor Suzdaliev <swindows101@gmail.com>
2020-02-07 12:02:37 +0200

    dfgh

--- sample/platform/linux/telemetry/main.cpp
+++ sample/platform/linux/telemetry/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char** argv)
 
   // Setup OSDK.
   std::cout << "\nwait 5 sec before vehicle init \n";
-  sleep(5);
+  usleep(5000000);
   std::cout << "\n start init\n";
   LinuxSetup linuxEnvironment(argc, argv);
   Vehicle*   vehicle = NULL;
