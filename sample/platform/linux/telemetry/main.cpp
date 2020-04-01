8b4667c819c551d52084f51ed844b87558d66b23
Ihor Suzdaliev <swindows101@gmail.com>
2020-04-01 13:00:57 +0300

    test no output

--- sample/platform/linux/telemetry/main.cpp
+++ sample/platform/linux/telemetry/main.cpp
@@ -115,14 +115,14 @@ int main(int argc, char** argv)
    std::thread telemetry_thr(subscribeToData, vehicle, std::ref(pitchGV), std::ref(rwbGV), std::ref(btnC1GV), std::ref(btnC2GV), std::ref(camGV));
   telemetry_thr.detach();
   std::thread control_thr(send_control, std::ref(pitchGV), std::ref(rwbGV), std::ref(btnC1GV), std::ref(btnC2GV), std::ref(camGV));
-  control_thr.detach();
-  
+  //control_thr.detach();
+  /*
   while(true){
           std::cout << "\n[main cycle] pitchGV = " << pitchGV << "\n" << std::endl;
           std::cout << "\n[main cycle] PWM = " << pitchGV + PITCH_CAL -24  << "\n" << std::endl;
           usleep(10000000);
   }
-     
+     */
   bcm2835_close();
   return 0;
 }
