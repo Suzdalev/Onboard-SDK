e3f37bff644196b0beed41ab51024fcffa4a9481
Ihor Suzdaliev <swindows101@gmail.com>
2020-07-02 14:12:05 +0300

    sfghdfgh

--- sample/platform/linux/telemetry/main.cpp
+++ sample/platform/linux/telemetry/main.cpp
@@ -80,6 +80,10 @@ void send_control(int& pitchGV, int& rwbGV, int& btnC1GV, int& btnC2GV, int& cam
    bcm2835_i2c_write(buffer.c_str(), buffer.length());
    usleep(1000);
    
+
+   if(camGV < 992){   //sbus center value of 1500pwm
+     camGV = 992;
+   }
    buffer = "z" + std::to_string(camGV);
    bcm2835_i2c_write(buffer.c_str(), buffer.length());
    usleep(1000);
